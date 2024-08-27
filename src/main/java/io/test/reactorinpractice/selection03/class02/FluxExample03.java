9b6c8fc84022fa6b34c13b606839c6b5658e816b
Joonwoo Park <93jpark@gmail.com>
2024-08-27 18:45:24 +0900

    basic Flux in practice

---
+++ src/main/java/io/test/reactorinpractice/selection03/class02/FluxExample03.java
@@ -0,0 +1,18 @@
+package io.test.reactorinpractice.selection03.class02;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * 2개의 Mono를 연결해서 Flux로 변환하는 예제
+ */
+@Slf4j
+public class FluxExample03 {
+    public static void main(String[] args) {
+        Flux<Object> flux =
+                Mono.justOrEmpty(null)
+                        .concatWith(Mono.justOrEmpty("Jobs")); // Mono와 파라미터의 Mono를 합쳐주는 연산자
+        flux.subscribe(data -> log.info("# result: {}", data));
+    }
+}
---
+++ src/main/java/io/test/reactorinpractice/selection03/class02/FluxExample04.java
@@ -0,0 +1,21 @@
+package io.test.reactorinpractice.selection03.class02;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * 여러 개의 Flux를 연결해서 하나의 Flux로 결합하는 예제
+ */
+@Slf4j
+public class FluxExample04 {
+    public static void main(String[] args) {
+        Flux.concat(
+                        Flux.just("Venus"),
+                        Flux.just("Earth"),
+                        Flux.just("Mars")
+                )
+                .collectList() // 한개의 리스트로 각 Flux들의 데이터를 합침
+                .subscribe(planetList -> log.info("# Solar System: {}", planetList));
+    }
+}
